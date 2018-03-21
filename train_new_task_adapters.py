846bdcbf2eb4880371bc7ad8566addaa86a82f0f
sylvestre <srebuffi@robots.ox.ac.uk>
2018-03-21 13:36:18 +0100

    auto

--- train_new_task_adapters.py
+++ train_new_task_adapters.py
@@ -105,9 +105,12 @@ for name, m in net_old.named_modules():
         store_data_rm.append(m.running_mean)
         store_data_rv.append(m.running_var)
 
+print(names)
+
 for id_task in range(len(num_classes)):
     element = 0
     for name, m in net.named_modules():
+        print(name)
         if isinstance(m, nn.BatchNorm2d) and 'bns.'+str(id_task) in name and name[:-1] in names:
                 print(name)
                 m.weight.data = store_data[element].clone()
